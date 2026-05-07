7cda1bfefcfaad265137e932ee0eb9c6bfb11ea4
gucio321 <gucio321@protonmail.com>
2026-05-07 09:24:15 +0200

    fix pylint

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
+"""
+this module does nothing
+"""
 
 
-def sum(x, y):
+def sum2(x, y):
+    """
+    sum2 sums two numbers
+    """
     return x + y
 
 
 def other(x):
+    """
+    other does other useless things
+    """
     print(x)
